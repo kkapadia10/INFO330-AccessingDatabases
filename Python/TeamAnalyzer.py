1ef2f65b6d6948e1216391fae5c55ddcf395a8eb
Kaden Kapadia <106453833+kkapadia10@users.noreply.github.com>
2023-05-02 18:14:44 -0700

    Finished HW -- extra credit not completed yet

--- Python/TeamAnalyzer.py
+++ Python/TeamAnalyzer.py
@@ -1,5 +1,8 @@
-import sqlite3  # This is the package for all sqlite3 access in Python
-import sys      # This helps with command-line parameters
+import sqlite3
+import sys
+
+connection = sqlite3.connect('../pokemon.sqlite')
+cursor = connection.cursor()
 
 # All the "against" column suffixes:
 types = ["bug","dark","dragon","electric","fairy","fight",
@@ -22,6 +25,38 @@ for i, arg in enumerate(sys.argv):
     # Remember to look at those "against_NNN" column values; greater than 1
     # means the Pokemon is strong against that type, and less than 1 means
     # the Pokemon is weak against that type
+for pokemon in sys.argv[1:]:
+    strong = []
+    weak = []
+    pokemon_name = cursor.execute(
+        "SELECT name FROM pokemon WHERE id=?", (pokemon,)).fetchone()
+    name = pokemon_name[0]
+    print("Analyzing " + str(pokemon))
+
+    ids = cursor.execute(
+        "SELECT type_id FROM pokemon_type WHERE pokemon_id=?", (pokemon,)).fetchall()
+    against = cursor.execute(
+        "SELECT * FROM against WHERE type_source_id1=? AND type_source_id2=?",
+        (ids[0][0], ids[1][0],)).fetchall()
+    val = against[0][2:]
+    for i in range(len(val)):
+        if val[i] > 1:
+            strong.append(types[i])
+        elif val[i] < 1:
+            weak.append(types[i])
+    list = []
+    for id in ids:
+        exec = cursor.execute(
+            "SELECT name FROM type WHERE id=?", (id[0],)).fetchone()
+        for ex in exec:
+            list.append(ex)
+    new_str = ""
+    for val in list:
+        new_str += val
+        new_str += " "
+    temp = new_str.rstrip()
+    print(name + " (" + temp + ") is strong against " + str(strong) +
+          " but weak against " + str(weak))
 
 answer = input("Would you like to save this team? (Y)es or (N)o: ")
 if answer.upper() == "Y" or answer.upper() == "YES":
@@ -31,4 +66,3 @@ if answer.upper() == "Y" or answer.upper() == "YES":
     print("Saving " + teamName + " ...")
 else:
     print("Bye for now!")
-
